3f804781f43ce214383c0a087d28c367b7079beb
Chase McManning <cmcmanning@gmail.com>
2015-08-11 16:52:08 -0400

    Adjustments to the PHP generator
    Changing ‘Pattern’ to ‘Template’ (makes more sense in context), adding
    the inherited resource class with comment blocks on exact expected
    format, and various bug fixes

--- drafts/php/compile.py
+++ drafts/php/compile.py
@@ -39,141 +39,258 @@ def generate_resource_php(resource):
     j2_env.globals['input_datetime_format'] = 'Y-m-d H:i:s'
 
     # Load resources into fragment files
-    template = j2_env.get_template('templates/base_resource.jinja')
+    base_template = j2_env.get_template('templates/base_resource.jinja')
+    impl_template = j2_env.get_template('templates/resource.jinja')
 
-    result = template.render(
-        properties=resource['properties']
-    )
+    with open('BaseGenerated.php', 'w') as f:
+        f.write(base_template.render(
+            properties=resource['properties']
+        ))
 
-    with open('generated.php', 'w') as f:
-        f.write(result)
+    with open('Generated.php', 'w') as f:
+        f.write(impl_template.render(
+            properties=resource['properties']
+        ))
 
-if __name__ == '__main__':
-    generate_resource_php({
-        "id": "TestResource",
-        "uri": "/test/{id}",
-        "type": "object",
-        "properties": {
-            "stringProp": {
+
+PERSON = { 
+    "id": "Person",
+    "uri": "/person/{id}",
+    "properties": {
+      "osuid": {
+        "type": "string"
+      },
+      "firstName": {
+        "type": "string"
+      },
+      "middleName": {
+        "type": "string"
+      },
+      "lastName": {
+        "type": "string"
+      },
+      "username": {
+        "type": "string"
+      },
+      "active": {
+        "type": "boolean"
+      },
+      "jobCode": {
+        "type": "string"
+      },
+      "jobDescription": {
+        "type": "string"
+      },
+      "jobGroup": {
+        "type": "string"
+      },
+      "apptCode": {
+        "type": "string"
+      },
+      "apptDescription": {
+        "type": "string"
+      },
+      "fte": {
+        "type": "number"
+      },
+      "coworkers": {
+        "type": "collection",
+        "collection": "PersonCoworkers",
+        "uri": "/person/{id}/coworkers"
+      },
+      "department": {
+        "type": "resource",
+        "resource": "Department",
+        "uri": "/department/{id}"
+      },
+      "otherDepartments": {
+        "type": "array",
+        "items": {
+          "type": "object",
+          "properties": {
+            "fte": {
+              "type": "number"
+            },
+            "department": {
+              "type": "resource",
+              "resource": "Department",
+              "uri": "/department/{id}"
+            }
+          }
+        }
+      },
+      "addresses": {
+        "type": "array",
+        "items": {
+          "type": "object",
+          "properties": {
+            "address1": {
+              "type": "string"
+            },
+            "address2": {
+              "type": "string"
+            },
+            "city": {
+              "type": "string"
+            },
+            "state": {
+              "type": "string"
+            },
+            "zip": {
+              "type": "string"
+            },
+            "room": {
+              "type": "string"
+            },
+            "building": {
+              "type": "string"
+            },
+            "phone": {
+              "type": "string"
+            }
+          }
+        }
+      }
+    }
+}
+
+TEST = {
+    "id": "TestResource",
+    "uri": "/test/{id}",
+    "type": "object",
+    "properties": {
+        "stringProp": {
+            "type": "string",
+            "description": "A generic string value"
+        },
+        "boolProp": {
+            "type": "boolean",
+            "description": "A boolean value"
+        },
+        "numberProp": {
+            "type": "number",
+            "description": "A numeric value"
+        },
+        "dateProp": {
+            "type": "string",
+            "format": "date",
+            "description": "A date value, stored as a PHP DateTime object"
+        },
+        "objectProp": {
+            "type": "object",
+            "description": "A complex object containing primitives or references",
+            "properties": {
+                "opStringProp": {
+                    "type": "string"
+                },
+                "opBoolProp": {
+                    "type": "boolean"
+                },
+                "opNumberProp": {
+                    "type": "number"
+                },
+                "opDateProp": {
+                    "type": "string",
+                    "format": "date"
+                },
+                "opResourceProp": {
+                    "type": "resource",
+                    "resource": "ObjectResource",
+                    "uri": "/object-resource/{id}"
+                },
+                "opCollectionProp": {
+                    "type": "collection",
+                    "collection": "ObjectCollection",
+                    "uri": "/object-collection/{id}"
+                }
+            }
+        },
+        "resourceProp": {
+            "type": "resource",
+            "resource": "TestResource",
+            "uri": "/resource/{id}",
+            "description": "A reference to another resource"
+        },
+        "collectionProp": {
+            "type": "collection",
+            "collection": "TestCollection",
+            "uri": "/collection/{id}",
+            "description": "A reference to a collection"
+        },
+        "arrayOfStringProp": {
+            "type": "array",
+            "description": "An unordered list of string values",
+            "items": {
+                "type": "string"
+            }
+        },
+        "arrayOfNumberProp": {
+            "type": "array",
+            "description": "An unordered list of numeric values",
+            "items": {
+                "type": "number"
+            }
+        },
+        "arrayOfDateProp": {
+            "type": "array",
+            "description": "An unordered list of DateTime objects",
+            "items": {
                 "type": "string",
-                "description": "A generic string value"
-            },
-            "boolProp": {
-                "type": "boolean",
-                "description": "A boolean value"
-            },
-            "numberProp": {
-                "type": "number",
-                "description": "A numeric value"
-            },
-            "dateProp": {
-                "type": "string",
-                "format": "date",
-                "description": "A date value, stored as a PHP DateTime object"
-            },
-            "objectProp": {
+                "format": "date"
+            }
+        },
+        "arrayOfResourceProp": {
+            "type": "array",
+            "description": "An unordered list of references to resources, all of the same type",
+            "items": {
+                "type": "resource",
+                "resource": "ArrayResource",
+                "uri": "/array-resource/{id}"
+            }
+        },
+        "arrayOfCollectionProp": {
+            "type": "array",
+            "description": "An unordered list of references to collections, all of the same type",
+            "items": {
+                "type": "collection",
+                "collection": "ArrayCollection",
+                "uri": "/array-collection/{id}"
+            }
+        },
+        "arrayOfObjectProp": {
+            "type": "array",
+            "description": "An unordered list of complex objects, each with the same schema containing primitives or references",
+            "items": {
                 "type": "object",
-                "description": "A complex object containing primitives or references",
                 "properties": {
-                    "opStringProp": {
+                    "aopStringProp": {
                         "type": "string"
                     },
-                    "opBoolProp": {
+                    "aopBoolProp": {
                         "type": "boolean"
                     },
-                    "opNumberProp": {
+                    "aopNumberProp": {
                         "type": "number"
                     },
-                    "opDateProp": {
+                    "aopDateProp": {
                         "type": "string",
                         "format": "date"
                     },
-                    "opResourceProp": {
+                    "aopResourceProp": {
                         "type": "resource",
+                        "resource": "ObjectResource",
                         "uri": "/object-resource/{id}"
                     },
-                    "opCollectionProp": {
+                    "aopCollectionProp": {
                         "type": "collection",
+                        "collection": "ObjectCollection",
                         "uri": "/object-collection/{id}"
-                    }
-                }
-            },
-            "resourceProp": {
-                "type": "resource",
-                "uri": "/resource/{id}",
-                "description": "A reference to another resource"
-            },
-            "collectionProp": {
-                "type": "collection",
-                "uri": "/collection/{id}",
-                "description": "A reference to a collection"
-            },
-            "arrayOfStringProp": {
-                "type": "array",
-                "description": "An unordered list of string values",
-                "items": {
-                    "type": "string"
-                }
-            },
-            "arrayOfNumberProp": {
-                "type": "array",
-                "description": "An unordered list of numeric values",
-                "items": {
-                    "type": "number"
-                }
-            },
-            "arrayOfDateProp": {
-                "type": "array",
-                "description": "An unordered list of DateTime objects",
-                "items": {
-                    "type": "string",
-                    "format": "date"
-                }
-            },
-            "arrayOfResourceProp": {
-                "type": "array",
-                "description": "An unordered list of references to resources, all of the same type",
-                "items": {
-                    "type": "resource",
-                    "uri": "/array-resource/{id}"
-                }
-            },
-            "arrayOfCollectionProp": {
-                "type": "array","description": "An unordered list of references to collections, all of the same type",
-                "items": {
-                    "type": "collection",
-                    "uri": "/array-collection/{id}"
-                }
-            },
-            "arrayOfObjectProp": {
-                "type": "array",
-                "description": "An unordered list of complex objects, each with the same schema containing primitives or references",
-                "items": {
-                    "type": "object",
-                    "properties": {
-                        "aopStringProp": {
-                            "type": "string"
-                        },
-                        "aopBoolProp": {
-                            "type": "boolean"
-                        },
-                        "aopNumberProp": {
-                            "type": "number"
-                        },
-                        "aopDateProp": {
-                            "type": "string",
-                            "format": "date"
-                        },
-                        "aopResourceProp": {
-                            "type": "resource",
-                            "uri": "/object-resource/{id}"
-                        },
-                        "aopCollectionProp": {
-                            "type": "collection",
-                            "uri": "/object-collection/{id}"
-                        }
                     }
                 }
             }
         }
-    })
+    }
+}
+
+if __name__ == '__main__':
+    generate_resource_php(TEST)
