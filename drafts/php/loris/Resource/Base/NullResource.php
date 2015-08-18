f19c00edef61247c4654065eecba88448ca2aeff
Chase McManning <cmcmanning@gmail.com>
2015-08-18 16:45:10 -0400

    Maintaining uri for null resources
    Justification; resource iterating becomes easier when we assume each
    one has a URI to reference. Otherwise, there’s additional testing
    against whether something is an instance of NullResource

--- drafts/php/loris/Resource/Base/NullResource.php
+++ drafts/php/loris/Resource/Base/NullResource.php
@@ -3,9 +3,14 @@ namespace Loris\Resource\Base;
 
 class NullResource extends Meta
 {
-    function __construct()
+    /**
+     * Note that $uri is passed in so that iterating a set of resources
+     * will still give us valid $uri values, regardless if we picked a 
+     * Meta, an instantiated one, or a Null instance. 
+     */
+    function __construct($uri)
     {
-        parent::__construct(null, null);
+        parent::__construct(null, $uri);
     }
 
     /**
@@ -13,6 +18,12 @@ class NullResource extends Meta
      */
     public function serialize()
     {
+        // TODO: Maybe expand this a bit and provide the requested URI
+        // and something more than null. (e.g. "This is what you requested,
+        // but it doesn't exist here") not necessarily a 404, since some
+        // relationships expect null (like a person's projects, if they have none)
+        // but others would indicate a bad relationship (e.g. invalid department 
+        // number in the backend)
         return null; // Nothing!
     }
 }
