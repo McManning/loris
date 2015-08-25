631876d8b75c1bcf8f18d3ba13373797a1598628
Chase McManning <cmcmanning@gmail.com>
2015-08-25 16:09:00 -0400

    Adding collection meta to the constructor
    page/limit values did not carry over when a collection was resolved
    from MetaCollection

--- drafts/php/loris/Resource/Base/MetaCollection.php
+++ drafts/php/loris/Resource/Base/MetaCollection.php
@@ -9,13 +9,13 @@ class MetaCollection extends Meta
     const DEFAULT_PAGE = 1;
     const DEFAULT_LIMIT = 10;
 
-    function __construct($id, $uri)
+    function __construct($id, $uri, $page = self::DEFAULT_PAGE, $limit = self::DEFAULT_LIMIT)
     {
         parent::__construct($id, $uri);
 
         $this->meta->type = 'collection';
-        $this->meta->page = self::DEFAULT_PAGE;
-        $this->meta->limit = self::DEFAULT_LIMIT;
+        $this->meta->page = $page;
+        $this->meta->limit = $limit;
         $this->meta->total = null;
     }
 }
